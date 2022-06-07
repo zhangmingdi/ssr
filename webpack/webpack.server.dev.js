97a30646c6aca811bab0ca984d68005dcd8024b4
zhangmingdi <zhangmingdi@shein.com>
2022-06-07 15:19:42 +0800

    格式化

--- webpack/webpack.server.dev.js
+++ webpack/webpack.server.dev.js
@@ -1,8 +1,8 @@
 const WebpackNodeExternals = require('webpack-node-externals');
 const path = require('path');
 const { merge } = require('webpack-merge');
-const base = require('./webpack.base');
 const webpack = require('webpack');
+const base = require('./webpack.base');
 
 module.exports = merge(base, {
   mode: 'development',
@@ -14,9 +14,9 @@ module.exports = merge(base, {
   },
   externals: [WebpackNodeExternals()], // 排除不需要的打包模块
 
-  plugins: [  // 配置webpack
+  plugins: [ // 配置webpack
     new webpack.DefinePlugin({
-      '__isServer': true,   // 服务端设置true，客户端设置false
+      __isServer: true, // 服务端设置true，客户端设置false
     }),
-  ]
+  ],
 });
