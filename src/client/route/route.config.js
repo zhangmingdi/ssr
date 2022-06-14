5c7a86c71d54fb5b14d0312b23019f38ea881bc1
zhangmingdi <zhangmingdi@shein.com>
2022-06-14 22:11:08 +0800

    动态加载组件

--- src/client/route/route.config.js
+++ src/client/route/route.config.js
@@ -1,5 +1,39 @@
+import React from 'react';
 import Home from '../pages/home';
-import Study from '../pages/study';
+// import Study from '../pages/study';
+
+const asyncComp = (load) => {
+  return class AsyncComp extends React.Component {
+    static _load = load;
+
+    constructor(props) {
+      super(props);
+      this.state = {
+        Comp: null,
+      };
+    }
+
+    componentDidMount() {
+      const { Comp } = this.state;
+      if (!Comp) {
+        console.log('加载异步组件');
+        this.asyncLoad();
+      }
+    }
+
+    asyncLoad = async () => {
+      const { default: Comp } = await AsyncComp._load();
+      this.setState({ Comp });
+    };
+
+    render() {
+      const { Comp } = this.state;
+      return (
+        Comp ? <Comp {...this.props} /> : <div>loading</div>
+      );
+    }
+  };
+};
 
 const arr = [
   {
@@ -14,7 +48,7 @@ const arr = [
     path: '/study',
     exact: true,
     name: 'Study',
-    component: Study,
+    component: asyncComp(() => { return import('../pages/study'); }),
   },
 ];
 
